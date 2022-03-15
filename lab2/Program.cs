ccf6ff3c782047b5ae0a326e0e1642b4c527beff
MilekJakub <jakub.milek@microsoft.wsei.edu.pl>
2022-03-15 10:36:29 +0100

    lab3

---
+++ lab2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ZADANIE 1");
+            int[] arr = { 2, 2, 2, 2, 2, 2 };
+            var sum = Zadanie1(arr, arr.Length);
+            Console.WriteLine($"Sum: {sum}");
+
+            Console.WriteLine("ZADANIE 2");
+            int[] arr2 = { 1, 2, 2, 2, 4, 6 };
+            var counter = Zadanie2(arr2, arr2.Length, 2);
+            Console.WriteLine(counter);
+
+            Console.WriteLine("ZADANIE 3");
+            Zadanie3(0,0); //Do poprawy, liczba 99 nie działa
+        }
+        static int Zadanie1(int[] tab, int tabLenght)
+        {
+            var sum = 0;
+            tabLenght -= 1;
+            if (tabLenght >= 0)
+                sum = tab[tabLenght] + Zadanie1(tab, tabLenght);
+            
+            return sum;
+        }
+        static int Zadanie2(int[] tab, int tabLenght, int element)
+        {
+            var counter = 0;
+            var sum = 0;
+            tabLenght -= 1;
+            if(tabLenght >= 0)
+            {
+                if (tab[tabLenght] == element)
+                    counter++;
+                sum = counter + Zadanie2(tab, tabLenght, element);
+            }
+            return sum;
+        }
+        static void Zadanie3(int start, int counter)
+        {
+            int edge = 100;
+            counter++;
+            start += 1;
+            if(edge >= start)
+            {
+                if (counter == 3 && start.ToString().EndsWith("0") || start.ToString().EndsWith("5"))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (start.ToString().EndsWith("0") || start.ToString().EndsWith("5"))
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (counter == 3)
+                {
+                    Console.WriteLine("Fizz");
+                    counter = 0;
+                }
+                else
+                    Console.WriteLine(start);
+                Zadanie3(start, counter);
+            }
+            return;
+        }
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab3
+{
+    public class SinusZadanie
+    {
+        private const int max = 360;
+        static public double[] sinTable = new double [max];
+        static SinusZadanie()
+        {
+            
+            //wypełnić sinTable dla degree od 0 do 359
+            for (int i = 0; i < max; i++)
+            {
+                sinTable[i] = Math.Sin(i * Math.PI / 180);
+            }
+        }
+        public static double Sin(int degree)
+        {
+            //zwróć sinTable od degree, ale dla dowolnych kątów
+            if (degree > 0)
+                return sinTable[degree % max];
+            else
+                return -sinTable[degree % max];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fibbonaci(42));
+            Console.WriteLine(SinusZadanie.Sin(0) == 0);
+            Console.WriteLine(SinusZadanie.Sin(90) == 1);
+            Console.WriteLine(SinusZadanie.Sin(180) == 0);
+            Console.WriteLine(SinusZadanie.Sin(-90) == -1);
+            Console.WriteLine(SinusZadanie.Sin(-180) == 0);
+        }
+        static long fib(int n, long[] mem)
+        {
+            if (n < 2)
+                return 1;
+            if (mem[n - 1] == -1)
+                mem[n - 1] = fib(n - 1, mem);
+            if (mem[n - 2] == -1)
+                mem[n - 2] = fib(n - 2, mem);
+            return mem[n - 1] + mem[n - 2];
+        }
+        static long fibbonaci(int n)
+        {
+            long[] mem = new long[n];
+            Array.Fill<long>(mem, -1);
+            return fib(n, mem);
+        }
+    }
+}
