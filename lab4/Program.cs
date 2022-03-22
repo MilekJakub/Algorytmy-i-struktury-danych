2194ec1dca39110730432fb728906ce5336252e9
MilekJakub <jakub.milek@microsoft.wsei.edu.pl>
2022-03-22 09:49:32 +0100

    Zadanie 1

---
+++ lab4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tablicaStringow = new string[] { "Jak", "Kuba", "Bogu", "tak", "Bóg", "Kubie" };
+            InsertionSort(tablicaStringow);
+        }
+        static void InsertionSort(string[] stringTab)
+        {
+            for (int i = 0; i < stringTab.Length - 1; i++)
+            {
+                for(int j = i + 1; j > 0; j--)
+                {
+                    if(stringTab[j - 1].CompareTo(stringTab[j]) == 1)
+                    {
+                        string temp = stringTab[j - 1];
+                        stringTab[j - 1] = stringTab[j];
+                        stringTab[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted array:");
+            foreach (var item in stringTab)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
