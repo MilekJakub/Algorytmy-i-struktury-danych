5c40bb486daaa51cbe8f8b12591e629f03d47fdd
MilekJakub <jakub.milek@microsoft.wsei.edu.pl>
2022-05-24 10:18:43 +0200

    lab 11 kod

--- lab11/Program.cs
+++ lab11/Program.cs
@@ -1,12 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace lab11
 {
+    interface IGraph
+    {
+        bool AddDirectedEdge(int source, int target, int weight);
+        bool AddUndirectedEdge(int source, int target, int weight);
+    }
+
+    class AdMatrix : IGraph
+    {
+        private int[,] _matrix;
+
+        public AdMatrix(int size)
+        {
+            _matrix = new int[size, size];
+        }
+
+        public bool AddDirectedEdge(int source, int target, int weight)
+        {
+            _matrix[source, target] = weight;
+            return true;
+        }
+
+        public bool AddUndirectedEdge(int source, int target, int weight)
+        {
+            return AddDirectedEdge(source, target, weight) && AddDirectedEdge(target, source, weight);
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            for (int row = 0; row < _matrix.GetLength(0); row++)
+            {
+                stringBuilder.Append($"wierzchołek {row} połączony z: ");
+                for (int col = 0; col < _matrix.GetLength(1); col++)
+                {
+                    if(_matrix[row, col] != 0)
+                        stringBuilder.Append(col + " ");
+                }
+                stringBuilder.Append("\n");
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+
+    class AdList : IGraph
+    {
+        Dictionary<int, HashSet<Edge>> _edges = new Dictionary<int, HashSet<Edge>>();
+
+        public bool AddDirectedEdge(int source, int target, int weight)
+        {
+            if (!_edges.ContainsKey(source))
+                _edges.Add(source, new HashSet<Edge>());
+
+            if (!_edges.ContainsKey(target))
+                _edges.Add(target, new HashSet<Edge>());
+
+            _edges[source].Add(new Edge() { Node = target, Weight = weight });
+
+            return true;
+        }
+
+        public bool AddUndirectedEdge(int source, int target, int weight)
+        {
+            return AddDirectedEdge(source, target, weight) && AddDirectedEdge(target, source, weight);
+        }
+
+        public void LevelTraversal(int source)
+        {
+
+        }
+    }
+
+    record Edge
+    {
+        public int Node { get; set; }
+        public double Weight { get; set; }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            IGraph graph = new AdMatrix(4);
+            graph.AddDirectedEdge(0, 1, 1);
+            graph.AddDirectedEdge(1, 3, 1);
+            graph.AddDirectedEdge(3, 0, 1);
+            graph.AddDirectedEdge(0, 2, 1);
+            graph.AddDirectedEdge(3, 2, 1);
+
+            Console.WriteLine(graph);
         }
     }
 }
---
+++ lab11/zadania.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab_11
+{
+
+    interface IGraph{
+        bool AddEdge(int start, int end, int weight = 1);
+        bool AddDirectedEdge(int s, int e, int w = 1);
+        bool RemoveEdge(int s, int e);
+        bool RemoveDirectedEdge(int s, int e);
+    }
+
+    class AdGraph:IGraph
+    {
+        int[,] _matrix;
+
+        public AdGraph(int n)
+        {
+            _matrix = new int[n, n];
+        }
+
+        public bool AddDirectedEdge(int start, int end, int weight = 1)
+        {
+            //kod testujący poprawność start i end
+            //zwróć false jeśli start lub end są poza zakresem indeksów _matrix
+            _matrix[start, end] = weight;
+            return true;
+        }
+
+        public bool AddEdge(int start, int end, int weight = 1)
+        {
+            return 
+                AddDirectedEdge(start, end, weight) 
+                && 
+                AddDirectedEdge(end, start, weight);
+
+        }
+
+        public bool RemoveDirectedEdge(int start, int end)
+        {
+            //kod testujący poprawność start i end
+            //zwróć false jeśli start lub end są poza zakresem indeksów _matrix
+            //sprawdź czy nie istnieje taka krawędź i zwróc false
+            _matrix[start, end] = 0;
+            return true;
+        }
+
+        public bool RemoveEdge(int start, int end)
+        {
+            return
+                RemoveDirectedEdge(start, end)
+                &&
+                RemoveDirectedEdge(end, start);
+        }
+
+        public override string ToString()
+        {
+            string r = "";
+            for (int row = 0; row < _matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < _matrix.GetLength(1); col++)
+                {
+                    if (_matrix[row, col] != 0)
+                    {
+                        r += $"edge between {row} and {col}\n";
+                    }
+                }
+            }
+            return r;
+        }
+    }
+
+
+    class AdListGraph : IGraph
+    {
+        internal int count;
+        private List<List<Edge>> _edges = new List<List<Edge>>();
+
+        public AdListGraph(int count)
+        {
+            this.count = count;
+            for(int i = 0; i < count; i++)
+            {
+                _edges.Add(new List<Edge>());
+            }
+        }
+
+        public bool AddDirectedEdge(int start, int end, int weight = 1)
+        {
+            if (_edges[start] == null)
+            {
+                _edges[start] = new List<Edge>();
+            }
+            _edges[start].Add(new Edge() { Desc = end, Weight = weight });
+            return true;
+        }
+
+        public bool AddEdge(int start, int end, int weight = 1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveDirectedEdge(int start, int end)
+        {
+            if (_edges[start] == null)
+            {
+                return false;
+            }
+            Edge edge = _edges[start].FirstOrDefault(e => e.Desc == end);
+            return _edges[start].Remove(edge);
+        }
+
+        public bool RemoveEdge(int s, int e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private class Edge: IComparable<Edge>
+        {
+            public int Desc { get; init; }
+            public int Weight { get; init; }
+
+            public int CompareTo(Edge other)
+            {
+                return Weight.CompareTo(other.Weight);
+            }
+        }
+
+        public override string ToString()
+        {
+            string r = "";
+            for(int vertex = 0; vertex < _edges.Count; vertex++)
+            {
+                List<Edge> edges = _edges[vertex];
+                foreach(Edge edge in edges)
+                {
+                    r += $"edge between {vertex} and {edge.Desc}\n";
+                }
+            }
+            return r;
+        }
+    }
+    class App
+    {
+        static void AppMain(string[] args)
+        {
+            Console.WriteLine("Adjacency matrix graph");
+            IGraph graph = new AdGraph(4);
+            graph.AddDirectedEdge(0, 1);
+            graph.AddDirectedEdge(1, 2);
+            graph.AddDirectedEdge(2, 0);
+            graph.AddDirectedEdge(2, 3);
+            Console.WriteLine(graph);
+            Console.WriteLine("Adjacency list graph");
+            graph = new AdListGraph(5);
+            graph.AddDirectedEdge(0, 1);
+            graph.AddDirectedEdge(1, 2);
+            graph.AddDirectedEdge(2, 0);
+            graph.AddDirectedEdge(2, 3);
+            Console.WriteLine(graph);
+        }
+    }
+}
